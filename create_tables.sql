db92da2fc5b4e7d8f3039c9a5c7056e34749701d
shawnrpnus <E0175613@u.nus.edu>
2019-11-18 00:39:27 -0800

    Add create table statements for branch, vType, vehicle and customer

--- create_tables.sql
+++ create_tables.sql
@@ -1,32 +1,79 @@
-create table reservation (
-	res_confNo char(10) not null PRIMARY KEY,
-	vehicleType_vtname varchar(9) not null,
-	customer_cellphone char(10) not null,
-	rental_fromDate date not null,
-	rental_fromTime time not null,
-	rental_toDate date not null,
-	rental_toTime time not null,
-	foreign key (vehicleType_vtname) references vehicleType,
-	foreign key (customer_cellphone) references customer,
-	foreign key (rental_fromDate, rental_fromTime, rental_toDate, rental_toTime) references rental
+create table reservation
+(
+    res_confNo         char(10)   not null PRIMARY KEY,
+    vehicleType_vtname varchar(9) not null,
+    customer_cellphone char(10)   not null,
+    rental_fromDate    date       not null,
+    rental_fromTime    time       not null,
+    rental_toDate      date       not null,
+    rental_toTime      time       not null,
+    foreign key (vehicleType_vtname) references vehicleType,
+    foreign key (customer_cellphone) references customer,
+    foreign key (rental_fromDate, rental_fromTime, rental_toDate, rental_toTime) references rental
 );
 
-create table rental (
-	rental_rid char(6) not null PRIMARY KEY,
-	vehicle_vid char(5) not null,
-	customer_cellphone char(10) not null,
-	rental_fromDate date not null,
-	rental_fromTime time not null,
-	rental_toDate date not null,
-	rental_toTime time not null,
-	rental_odometer int not null,
-	rental_cardName varchar(20) not null,
-	rental_cardNo char(16) not null,
-	rental_ExpDate char(4) not null,
-	res_confNo char(10),
-	foreign key (vehicle_vid) references vehicle,
-	foreign key (customer_cellphone) references customer,
-	foreign key (res_confNo) references reservation
+create table rental
+(
+    rental_rid         char(6)     not null PRIMARY KEY,
+    vehicle_vid        char(5)     not null,
+    customer_cellphone char(10)    not null,
+    rental_fromDate    date        not null,
+    rental_fromTime    time        not null,
+    rental_toDate      date        not null,
+    rental_toTime      time        not null,
+    rental_odometer    int         not null,
+    rental_cardName    varchar(20) not null,
+    rental_cardNo      char(16)    not null,
+    rental_ExpDate     char(4)     not null,
+    res_confNo         char(10),
+    foreign key (vehicle_vid) references vehicle,
+    foreign key (customer_cellphone) references customer,
+    foreign key (res_confNo) references reservation
+);
+
+create table vehicleType
+(
+    vtName   char(20)      not null PRIMARY KEY,
+    features char(50)      not null,
+    hrate    number(10, 2) not null,
+    drate    number(10, 2) not null,
+    wrate    number(10, 2) not null,
+    hirate   number(10, 2) not null,
+    dirate   number(10, 2) not null,
+    wirate   number(10, 2) not null,
+    krate    number(10, 2) not null
+);
+
+create table customer
+(
+    cellphone number(11) not null PRIMARY KEY,
+    name char(50) not null,
+    address char(50) not null,
+    dlicense char(20) not null
+);
+
+create table vehicle
+(
+    vid integer not null PRIMARY KEY,
+    vlicense char(10) not null,
+    make char(20) not null,
+    model char(20) not null,
+    year number(4,0) not null,
+    color char(10) not null,
+    odometer number(10,2) not null,
+    status char(10) not null,
+    vtName char(20) not null,
+    location char(20) not null,
+    city char(20) not null,
+    foreign key (vtName) references vehicleType,
+    foreign key (location, city) references branch
+);
+
+create table branch
+(
+    location char(20),
+    city char(20),
+    PRIMARY KEY (location, city)
 );
 
 /* TODO, still just example tables
