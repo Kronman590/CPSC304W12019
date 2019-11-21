78414806bacae8cfe861acad4bf89a93ce5895de
shawnrpnus <E0175613@u.nus.edu>
2019-11-21 00:42:14 -0800

    Add insertReservation and getAvailableVehicleDetails methods

--- src/ca/ubc/cs304/database/CustomerOperations.java
+++ src/ca/ubc/cs304/database/CustomerOperations.java
@@ -3,11 +3,15 @@ package ca.ubc.cs304.database;
 /* this class handles all customer operations */
 
 import ca.ubc.cs304.model.CustomerModel;
+import ca.ubc.cs304.model.ReservationModel;
+import ca.ubc.cs304.model.VehicleDetailsModel;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class CustomerOperations {
     private static final String EXCEPTION_TAG = "[EXCEPTION]";
@@ -33,9 +37,30 @@ public class CustomerOperations {
         }
     }
 
+    public void insertReservation(Connection connection, ReservationModel model){
+        try {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO reservation VALUES (?, ?, ?, ?, ?, ?, ?)");
+            ps.setString(1, model.getconfNo());
+            ps.setString(2, model.getVtname());
+            ps.setString(3, model.getDlicense());
+            ps.setString(4, model.getFromDate());
+            ps.setString(5, model.getFromTime());
+            ps.setString(6, model.getToDate());
+            ps.setString(7, model.getToTime());
+
+            ps.executeUpdate();
+            connection.commit();
+
+            ps.close();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+            rollbackConnection(connection);
+        }
+    }
+
     //format: MM/DD/YYYY and HH:mm
     public void countAvailableVehicles(Connection connection, String vtname, String location, String fromDate,
-                                        String fromTime, String toDate, String toTime) {
+                                       String fromTime, String toDate, String toTime) {
         try {
             int count = 0;
             //First count vehicles that have no rentals
@@ -51,7 +76,7 @@ public class CustomerOperations {
             ps1.setString(2, location);
 
             ResultSet rs1 = ps1.executeQuery();
-            while(rs1.next()){
+            while (rs1.next()) {
                 count += rs1.getInt(1);
             }
 
@@ -72,7 +97,7 @@ public class CustomerOperations {
             ps2.setString(4, inputToDateTime);
 
             ResultSet rs2 = ps2.executeQuery();
-            while(rs2.next()){
+            while (rs2.next()) {
                 count += rs2.getInt(1);
             }
 
@@ -83,8 +108,60 @@ public class CustomerOperations {
         }
     }
 
+    public void getAvailableVehicleDetails(Connection connection, String vtname, String location, String fromDate,
+                                           String fromTime, String toDate, String toTime) {
+        try {
+            List<VehicleDetailsModel> vehicleDetailsList = new LinkedList<>();
+            PreparedStatement ps1 = connection.prepareStatement(
+                    "SELECT vehicle.MAKE, vehicle.model, vehicle.year, vehicle.COLOR, vehicle.VTNAME " +
+                            "FROM vehicle, rental " +
+                            "WHERE vehicle.VLICENSE NOT IN (SELECT rental.VLICENSE FROM rental) " +
+                            "AND vehicle.vtname = ? " +
+                            "AND vehicle.LOCATION = ?" +
+                            "AND vehicle.STATUS = 'available'"
+            );
+            ps1.setString(1, vtname);
+            ps1.setString(2, location);
+
+            ResultSet rs1 = ps1.executeQuery();
+            while (rs1.next()) {
+                VehicleDetailsModel vehicleDetails = new VehicleDetailsModel(rs1.getString("make"),
+                        rs1.getString("model"),
+                        rs1.getInt("year"),
+                        rs1.getString("color"),
+                        rs1.getString("vtname"));
+                vehicleDetailsList.add(vehicleDetails);
+            }
+
+            PreparedStatement ps2 = connection.prepareStatement(
+                    "SELECT vehicle.MAKE, vehicle.model, vehicle.year, vehicle.COLOR, vehicle.VTNAME " +
+                            "FROM vehicle, rental " +
+                            "WHERE rental.VLICENSE = vehicle.VLICENSE " +
+                            "AND vehicle.VTNAME = ? " +
+                            "AND vehicle.LOCATION = ?" +
+                            "AND (TO_DATE(?, 'MM/DD/YYYY HH24:MI') < TO_DATE(CONCAT(CONCAT(rental.RENTAL_FROMDATE, ' '), rental.RENTAL_FROMTIME), 'MM/DD/YYYY HH24:MI') " +
+                            "OR TO_DATE(?, 'MM/DD/YYYY HH24:MI') > TO_DATE(CONCAT(CONCAT(rental.RENTAL_TODATE, ' '), rental.RENTAL_TOTIME), 'MM/DD/YYYY HH24:MI'))");
+            ResultSet rs2 = ps2.executeQuery();
+            while (rs2.next()) {
+                VehicleDetailsModel vehicleDetails = new VehicleDetailsModel(rs1.getString("make"),
+                        rs1.getString("model"),
+                        rs1.getInt("year"),
+                        rs1.getString("color"),
+                        rs1.getString("vtname"));
+                vehicleDetailsList.add(vehicleDetails);
+            }
+
+            System.out.println("Details of available vehicles: ");
+            for (VehicleDetailsModel v : vehicleDetailsList){
+                System.out.println(v.toString());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void rollbackConnection(Connection connection) {
-        try  {
+        try {
             connection.rollback();
         } catch (SQLException e) {
             System.out.println(EXCEPTION_TAG + " " + e.getMessage());
